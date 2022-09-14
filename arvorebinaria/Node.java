81e4bdb058fd2563f810d300412f80d24dad6777
Giovanni Sencioles <66632486+Sensacioles@users.noreply.github.com>
2022-09-14 14:10:08 -0300

    add tpa_arvorebinaria

---
+++ arvorebinaria/Node.java
@@ -0,0 +1,32 @@
+/**@author Emanuel Norjosa Luz e Giovanni Sencioles*/
+
+package arvorebinaria;
+
+public class Node<Tipo> {
+    Tipo valor;
+    Node<Tipo> noEsquerdo;
+    Node<Tipo> noDireito;
+    Node(Tipo val){
+        this.valor = val;
+        this.noEsquerdo = null;
+        this.noDireito = null;
+    }
+    Tipo getValor(){
+        return this.valor;
+    } 
+    Node<Tipo> getEsquerdo(){
+        return this.noEsquerdo;
+    }
+    Node<Tipo> getDireito(){
+        return this.noDireito;
+    }
+    void setValor(Tipo val){
+        this.valor = val;
+    }
+    void setEsquerdo(Node<Tipo> node){
+        this.noEsquerdo = node;
+    }
+    void setDireito(Node<Tipo> node){
+        this.noDireito = node;
+    }
+}
